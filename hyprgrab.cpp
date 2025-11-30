7c4c863977e7cab3e7d7897d1e002258fe840ff8
luengor <aluengor0001@gmail.com>
2025-11-30 21:25:57 +0100

    static buffer

--- hyprgrab.cpp
+++ hyprgrab.cpp
@@ -46,7 +46,7 @@ void error(const std::string &msg) {
 }
 
 std::string exec_command(const std::string &command) {
-    std::array<char, 4096> buffer;
+    static std::array<char, 4096> buffer;
     std::string result;
     std::unique_ptr<FILE, void (*)(FILE *)> pipe(
         popen(command.c_str(), "r"),
